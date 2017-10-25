67c9273ea799ffee9048caaaa13ddf57482e0d7c
TheWhiteHawk <unconfigured@null.spigotmc.org>
2017-10-25 17:02:38 +0200

    Test

--- Biljarts/main.cpp
+++ Biljarts/main.cpp
@@ -4,7 +4,16 @@
 
 using namespace std;
 
-int main(){
+void PrintVector (const vector<double>& v){
+  //vector<int> v;
+  for (int i=0; i<v.size();i++){
+    cout << v[i] << endl;
+  }
+}
 
+int main(){
+    std::vector<double> TestVector = {7, 5, 16, 8};
+     PrintVector(TestVector);
+    //TestVector = vectorMagnitude(TestVector);
     return 0;
 }
