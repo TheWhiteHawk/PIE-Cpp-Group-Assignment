0ff4e1b0441f342b2fdd51df79bb3820fdbcdc5c
bk <GitHubBK@grr.la>
2017-11-12 16:12:34 +0100

    score function

--- Biljarts/main.cpp
+++ Biljarts/main.cpp
@@ -7,24 +7,60 @@
 
 using namespace std;
 
-int main(){
-    vector<double> v {0.5,0};
-    vector<double> in {1,1};
-    vector<double> out {0,0};
-    double cos = vectorCos(v);
-    double sin = vectorSin(v);
 
-    cout << cos << "\t" << sin << endl;
-    /*vector<vector<double>> M { {1,2} , {3,4} };
-    //matrixPrint(M);
-    transformMatrix(cos,sin,M);
-    //matrixPrint(M);
-    vectorMatrixProduct(in,M,out);
-    vectorPrint(out);
-    transformMatrixInv(cos,sin,M);
-    vector<double> out2 {0,0};
-    vectorMatrixProduct(out,M,out2);
-    vectorPrint(out2);*/
+void score(vector<Ball> &balls,vector<Ball> &pocketBalls){
+    TempTable table;
+    double W = table.TABLE_WIDTH;
+    double H = table.TABLE_HEIGHT;
+    double cornerX = table.CORNER_X;
+    double cornerY = table.CORNER_Y;
+
+    double A = pocketBalls[0].getPosition()[0];
+    double R = pocketBalls[0].getRadius();
+
+    for (int i = 0; i < balls.size(); i++){
+        bool hasScored = false;
+        double x = balls[i].getPosition()[0];
+        double y = balls[i].getPosition()[1];
+
+        //If statement for individual pockets
+        //Top left pocket
+        if (y+x <= A - R){
+            hasScored = true;
+        }
+        //Top middle pocket
+        if (y <= cornerY - R){
+            hasScored = true;
+        }
+        //Top right pocket
+        if (y-x <= -A - R - cornerX - H){
+            hasScored = true;
+        }
+        //Bottom left pocket
+        if (y-x >= -A + R + cornerY + W){
+            hasScored = true;
+        }
+        //Bottom middle pocket
+        if (y >= cornerY + W + R){
+            hasScored = true;
+        }
+        //Bottom right pocket
+        if (y+x >= -A + R + cornerX + H + cornerY + W){
+            hasScored = true;
+        }
+
+        if (hasScored){
+            balls[i].setHasScored(hasScored);
+            cout << "Ball " << balls[i].getBallNumber() << " has scored" << endl;
+        }
+    }
+}
+
+
+
+
+
+int main(){
     return 0;
 }
 
